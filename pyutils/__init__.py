49a681afab7ebc69088a2917d32f92cc1bb356e3
zhouxianggen <zhouxianggen@isyscore.com>
2019-08-31 12:43:02 +0800

    update

--- pyutils/__init__.py
+++ pyutils/__init__.py
@@ -5,6 +5,7 @@ from .network_utils import get_ip
 from .json_utils import JsonEncoder
 from .config_utils import Config
 from .imp_utils import imp_subclasses
+from .hash64 import hash64
 
 
-__all__ = ('get_ip', 'JsonEncoder', 'Config', 'imp_subclasses')
+__all__ = ('get_ip', 'JsonEncoder', 'Config', 'imp_subclasses', 'hash64')
--- pyutils/test.py
+++ pyutils/test.py
@@ -5,6 +5,8 @@ from pyobject import PyObject
 
 from config_utils import Config
 from imp_utils import imp_subclasses
+from hash64 import hash64
 
 
-print(imp_subclasses('/root/work/databus/databus/beat/bar.py', PyObject))
+print(hash64('abcde'))
+#print(imp_subclasses('/root/work/databus/databus/beat/bar.py', PyObject))
